2f642518a2c5821287381a96fb309b2626986091
alam0rt <samlockart@gmail.com>
2021-04-27 10:26:21 +1000

    init

---
+++ cmd/kubectl-doktor.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os"
+
+	"github.com/spf13/pflag"
+
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"github.com/alam0rt/kubectl-doctor/pkg/cmd"
+)
+
+func main() {
+	flags := pflag.NewFlagSet("kubectl-ns", pflag.ExitOnError)
+	pflag.CommandLine = flags
+
+	root := cmd.NewCmdNamespace(genericclioptions.IOStreams{In: os.Stdin, Out: os.Stdout, ErrOut: os.Stderr})
+	if err := root.Execute(); err != nil {
+		os.Exit(1)
+}
